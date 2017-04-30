16521c2476f0a92540302c7e7920aa6b48e450ea
Tasha <Tasha@192.168.7.3>
2017-05-01 01:26:05 +0300

    myproj

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Tasha
+ *
+ * Created on 1 мая 2017 г., 0:26
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hello world"<< endl;
+/*    system("pause");*/
+    return 0;
+}
+
