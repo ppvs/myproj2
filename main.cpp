518e8b82f91e37a938b09fcea9ebd4afff778ffc
ppvs <ppvsster@gmail.com>
2017-05-01 19:56:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -22,6 +22,7 @@ using namespace std;
 int main(int argc, char** argv) {
     cout << "Hello world"<< endl;
     cout << "YoHoHo"<< endl;
+    cout << "YoHoHo"<< endl;
 /*    system("pause");*/
     return 0;
 }
